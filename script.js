7acb4b1f47027e2610088f76565540a3fc81d293
sairah814 <sairah814@gmail.com>
2016-04-24 16:59:19 -0400

    beginning create drink function

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
 $(document).ready(function () {
-    /*
-    Strong ingredients: "Glug of rum", "slug of whisky", "splash of gin"
-    Salty ingredients: "Olive on a stick", "salt-dusted rim", "rasher of bacon"
-    Bitter ingredients: "Shake of bitters", "splash of tonic", "twist of lemon peel"
-    Sweet ingredients: "Sugar cube", "spoonful of honey", "splash of cola"
-    Fruity ingredients: "Slice of orange", "dash of cassis", "cherry on top"
-    */
 
-    /*
-    first randomly generate a number
-
-    */
-
-    var randomIndex = function (max, min) { //generates a random number between the range provided; will be used to randomly choose an ingredient from each of the pantry categories
+    function randomIndex(max, min) { //generates a random number between the range provided; will be used to randomly choose an ingredient from each of the pantry categories
         var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
         return randomNumber;
-    }
-
-    function testFunction(text) {
-        var message = "This is the " + text;
-        return message;
     }
 
     var Pantry = {
@@ -30,26 +13,13 @@ $(document).ready(function () {
         fruity: ["Slice of orange", "dash of cassis", "cherry on top"]
     }
 
-    var userAnswers = [];
-    var userPreferences = [];
-    var pantrysize = Object.keys(Pantry).length;
-    $("#submit").click(function () {
-        var index = 0;
-        $('select').each(function () {
-            userAnswers.push($(this).val());
-            if (userAnswers[index] === 'yes') {
-                //console.log(index);
-                userPreferences.push($(this).attr('name'));
-                //console.log("Current preferences are" + userPreferences + "because of " + userAnswers[index]);
-                index += 1;
-            } else {
-                index += 1;
-            }
-        });
-        var pantrychoice = randomIndex(2, 0);
-        console.log(pantrychoice);
-        var testmessage = testFunction("hello");
-        console.log(testmessage);
+    function createDrink {
+        for (i = 0; i < userPreferences.length; i++) {
+            var preference = userPreferences[i]; //records the preference like "salty"
+            var pantrychoice = randomIndex(2, 0);
+
+        }
+        //console.log(pantrychoice);
         //console.log(userAnswers);
         //console.log(userPreferences);
         /*var concoction = [];
@@ -58,5 +28,23 @@ $(document).ready(function () {
             var pantrychoice = randomIndex(2, 0);
             console.log(pantrychoice);
         }*/
+    }
+
+    var userAnswers = [];
+    var userPreferences = [];
+    var pantrysize = Object.keys(Pantry).length;
+    $("#submit").click(function () {
+        var index = 0;
+        $('select').each(function () {
+            userAnswers.push($(this).val());
+            if (userAnswers[index] === 'yes') {
+                userPreferences.push($(this).attr('name'));
+                index += 1;
+            } else {
+                index += 1;
+            }
+        });
+        //now call the createdrink function
+        //then call the display drink function
     });
 });
